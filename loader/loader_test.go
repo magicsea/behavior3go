063f353a8be12d9b4babc9b2b8183dececc0cf52
magicsea <magicsea_wy@163.com>
2017-07-28 14:18:00 +0800

    更新示例

--- loader/loader_test.go
+++ loader/loader_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	//b3 "github.com/magicsea/behavior3go"
+	b3 "github.com/magicsea/behavior3go"
 	//. "github.com/magicsea/behavior3go/actions"
 	//. "github.com/magicsea/behavior3go/composites"
 	. "github.com/magicsea/behavior3go/config"
@@ -31,13 +31,37 @@ func TestExample(t *testing.T) {
 
 }
 
+///////////////////////加载事例///////////////////////////
+//自定义action节点
+type LogTest struct {
+	Action
+	info string
+}
+
+func (this *LogTest) Initialize(setting *BTNodeCfg) {
+	this.Action.Initialize(setting)
+	this.info = setting.GetPropertyAsString("info")
+}
+
+func (this *LogTest) OnTick(tick *Tick) b3.Status {
+	fmt.Println("logtest:", this.info)
+	return b3.SUCCESS
+}
+
 func TestLoadTree(t *testing.T) {
 	treeConfig, ok := LoadTreeCfg("tree.json")
 	if ok {
-		tree := CreateBevTreeFromConfig(treeConfig, nil)
+		//自定义节点注册
+		maps := b3.NewRegisterStructMaps()
+		maps.Register("LogTest", new(LogTest))
+
+		//载入
+		tree := CreateBevTreeFromConfig(treeConfig, maps)
 		tree.Print()
 
+		//输入板
 		board := NewBlackboard()
+		//循环每一帧
 		for i := 0; i < 5; i++ {
 			tree.Tick(i, board)
 		}
